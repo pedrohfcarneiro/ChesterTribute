4948aa97606473d77f0bc8037f2b7dcc10d27510
pedrohfcarneiro <phtriplox8090@gmail.com>
2022-06-02 23:12:20 -0300

    Teste de inserção do player como widget no site.

---
+++ js/music-list.js
@@ -0,0 +1,44 @@
+let allMusic = [
+    {
+        name: "In The End",
+        artist: "Linkin Park",
+        img: "InTheEnd.jpg",
+        src: "InTheEnd"
+    },
+    {
+        name: "Lying From You",
+        artist: "Linkin Park",
+        img: "LyingFromYou.jpg",
+        src: "LyingFromYou"
+    },
+    {
+        name: "What I've Done",
+        artist: "Linkin Park",
+        img: "WhatI'veDone2.jpg",
+        src: "WhatIveDone"
+    },
+    {
+        name: "Numb/Encore",
+        artist: "Linkin Park feat. Jay-Z",
+        img: "NumbEncore.jpg",
+        src: "Numb_Encore"
+    },
+    {
+        name: "Shadow Of The Day",
+        artist: "Linkin Park",
+        img: "Shadow.jpg",
+        src: "ShadowOfTheDay"
+    },
+    {
+        name: "Breaking the Habit",
+        artist: "Linkin Park",
+        img: "Breaking.jpg",
+        src: "BreakingTheHabit"
+    },
+    {
+        name: "Leave Out All The Rest",
+        artist: "Linkin Park",
+        img: "Leave.jpg",
+        src: "LeaveOutAllTheRest"
+    }
+]
